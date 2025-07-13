6bacf18a94e9a72ec56f7d920ed833454c2e3342
바키찬 <ckstmznf@naver.com>
2025-07-13 16:57:50 +0900

    Feat: 시설 검색을 url query로 사용

--- src/page/FacilityListPage.tsx
+++ src/page/FacilityListPage.tsx
@@ -8,6 +8,7 @@ import {FacilityListItem} from "../component/atom/FacilityListItem";
 import {FacilitySearchReq, FacilityType} from "../types/facility";
 import {ChipSelect} from "../component/atom/ChipSelect";
 import {Button} from "../component/atom/Button";
+import {useSearchParams} from "react-router-dom";
 
 
 export type FacilityListPageProps = {}
@@ -15,20 +16,39 @@ export type FacilityListPageProps = {}
 const FacilityListPageStyle = styled.div`
 `
 
+function useFacilitySearch() : {searchReq: FacilitySearchReq, updateSearchParam : (key : string, value : string) => void}{
+    const [params, setParams] = useSearchParams();
+
+    //todo: 이 방법이 최선일까?
+    const searchReq : FacilitySearchReq = {
+        query: params.get('query') ?? '',
+        address: params.get('address') ?? '',
+        facilityType: params.get('facilityType') as FacilityType ?? '',
+        grade: params.get('grade') ?? '',
+        minPrice: Number(params.get('minPrice') ?? 0),
+        maxPrice: Number(params.get('maxPrice') ?? 100000000),
+        withinYears: Number(params.get('withinYears') ?? 0),
+        page: Number(params.get('page') ?? 1),
+        size: Number(params.get('size') ?? 20),
+    };
+    const updateSearchParam = (key: string, value: string | number) => {
+        const next = new URLSearchParams(params);
+        next.set(key, String(value));
+        setParams(next, {replace: true});
+    };
+
+    return {searchReq, updateSearchParam}
+}
+
 export const FacilityListPage: FC<FacilityListPageProps> = () => {
     const {searchFacility} = useFacility();
-
-    const [searchReq, setSearchReq] = useState<FacilitySearchReq>({})
+    const {searchReq, updateSearchParam} = useFacilitySearch()
 
     const {isLoading, data, error, fetchStatus} = useQuery({
         queryKey: ["facilities", searchReq],
         queryFn: async () => await searchFacility(searchReq),
         staleTime: 1000 * 3,
     })
-
-    useEffect(() => {
-        console.log(searchReq)
-    }, [searchReq])
 
     return (
         <PageLayout>
@@ -37,21 +57,21 @@ export const FacilityListPage: FC<FacilityListPageProps> = () => {
                     name={"시설 유형"}
                     label={"시설 유형"}
                     options={["양로원", "요양원", "단기보호", "방문간호", "방문요양", "방문목욕", "주야간보호"]}
-                    onChange={o => setSearchReq({...searchReq, "facilityType" : o as FacilityType})}
+                    onChange={o => updateSearchParam("facilityType", o)}
                 />
 
                 <ChipSelect
                     name={"시설 등급"}
                     label={"시설 등급"}
-                    options={["A등급", "B등급", "C등급", "D등급", "E등급"]}
-                    onChange={o => setSearchReq({...searchReq, "grade" : o})}
+                    options={["A", "B", "C", "급", "E"]}
+                    onChange={o => updateSearchParam("grade", o)}
                 />
 
                 <ChipSelect
                     name={"설립 연로"}
                     label={"설립 연도"}
                     options={["1", "3", "5", "10"]}
-                    onChange={o => setSearchReq({...searchReq, "withinYears" : Number(o)})}
+                    onChange={o => updateSearchParam("withinYears", o)}
                 />
 
                 {isLoading && "로딩중"}
