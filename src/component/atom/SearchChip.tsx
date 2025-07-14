4c8c23041e30efe96aa6e4fc543d3c9d800c9cd1
바키찬 <ckstmznf@naver.com>
2025-07-14 20:01:41 +0900

    Feat: SearchChip에서 undefined를 받도록

--- src/component/atom/SearchChip.tsx
+++ src/component/atom/SearchChip.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 export type SearchChipProps = {
     label: string;
-    value: string;
+    value: string | undefined;
     onClick: () => void;
 }
 const SearchChipStyle = styled.div<{ isActive: boolean }>`
