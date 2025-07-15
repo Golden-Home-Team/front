303a362c657fda29c24456de7d3161273d95593c
바키찬 <ckstmznf@naver.com>
2025-07-16 06:17:49 +0900

    Feat: HomePage Menu Item

---
+++ src/HomeMenuItem.tsx
@@ -0,0 +1,39 @@
+import {FacilityType} from "./domains/facility/types/facility";
+import {FC} from "react";
+import {allIcons} from "./common/assets/facilityIcon";
+import {MenuItemType} from "./common/page/MainPage";
+import styled from "styled-components";
+import {Space} from "./common/style/Space";
+
+export type HomeMenuItemProps = {
+    type: FacilityType | MenuItemType
+}
+const HomeMenuItemStyle = styled.li`
+  padding: 12px;
+  
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  
+  cursor: pointer;
+`
+const Icon = styled.img`
+  width: 33px;
+  height: 33px;
+`
+const Name = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 22px;
+  text-align: center;
+  white-space: nowrap;
+`
+export const HomeMenuItem: FC<HomeMenuItemProps> = ({type}) => {
+    return (
+        <HomeMenuItemStyle>
+            <Icon src={allIcons[type]} alt=""/>
+            <Space v={5}/>
+            <Name>{type}</Name>
+        </HomeMenuItemStyle>
+    );
+};
--- src/common/page/MainPage.tsx
+++ src/common/page/MainPage.tsx
@@ -2,22 +2,41 @@ import {FC} from "react";
 import styled from "styled-components";
 import {PageLayout} from "../style/PageLayout";
 import {MobileLayout} from "../../MobileLayout";
-import {Button} from "../../component/atom/Button";
-import {Link} from "react-router-dom";
-import {RoutePath} from "../../RoutePath";
 import {useFacility} from "../../domains/facility/context/FacilityContext";
 import {HomeAppBar} from "../../component/molecules/HomeAppBar";
+import {FacilityType} from "../../domains/facility/types/facility";
+import {HomeMenuItem} from "../../HomeMenuItem";
 
+export type MenuItemType = "맞춤추천" | "요양정보" | "지도검색";
 
 export type MainPageProps = {}
 
 const MainPageStyle = styled.div`
 `
 
+const HomeMenuList = styled.ul`
+    display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`
+
 
 export const MainPage: FC<MainPageProps> = () => {
     const {getFacility} = useFacility();
 
+    const facilities: (FacilityType | MenuItemType)[] = [
+        "맞춤추천",
+        "요양정보",
+        "지도검색",
+        "요양원",
+        "요양병원",
+        "양로원",
+        "실버타운",
+        "단기보호",
+        "주야간보호",
+        "방문요양",
+        "방문간호",
+        "방문목욕",
+    ];
 
     return (
         <PageLayout>
@@ -26,23 +45,11 @@ export const MainPage: FC<MainPageProps> = () => {
                     <HomeAppBar/>
                 )}
             >
-                <Link to={RoutePath.signUp}>
-                    <Button onClick={() => {
-                    }}>회원가입</Button>
-                </Link>
-
-                <Link to={RoutePath.login}>
-                    <Button onClick={() => {
-                    }}>로그인</Button>
-                </Link>
-
-                <Button onClick={() => {
-                    // const data = getFacilities("양로원", null, null)
-                    // console.log(data)
-                    console.log(getFacility(1))
-                }}>
-                    클릭
-                </Button>
+                <HomeMenuList>
+                    {facilities.map(type => (
+                        <HomeMenuItem type={type}/>
+                    ))}
+                </HomeMenuList>
             </MobileLayout>
         </PageLayout>
     );
